b94018b7ced4ccce889808465ac1f1cd965786eb
algonacci <algonacci1@gmail.com>
2022-10-17 23:38:37 +0700

    add data

--- src/scraping.py
+++ src/scraping.py
@@ -6,18 +6,31 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-q", "--query", required=True, help="news keyword")
+ap.add_argument("-t", "--topic", required=True, help="news topic")
 args = vars(ap.parse_args())
 
 print("""
- ___ _   _ _   _ _____    _    ____  
-|_ _| \ | | | | | ____|  / \  |  _ \ 
- | ||  \| | |_| |  _|   / _ \ | | | |
- | || |\  |  _  | |___ / ___ \| |_| |
-|___|_| \_|_| |_|_____/_/   \_\____/ 
+                    ____                       _             
+                   / ___|  ___ _ __ __ _ _ __ (_)_ __   __ _ 
+                   \___ \ / __| '__/ _` | '_ \| | '_ \ / _` |
+                    ___) | (__| | | (_| | |_) | | | | | (_| |
+                   |____/ \___|_|  \__,_| .__/|_|_| |_|\__, |
+                                        |_|            |___/ 
+                                       _ _   _     
+                             __      _(_) |_| |__  
+                             \ \ /\ / / | __| '_ \ 
+                              \ V  V /| | |_| | | |
+                               \_/\_/ |_|\__|_| |_|
+                                                   
+                      ___ _   _ _   _ _____    _    ____  
+                     |_ _| \ | | | | | ____|  / \  |  _ \ 
+                      | ||  \| | |_| |  _|   / _ \ | | | |
+                      | || |\  |  _  | |___ / ___ \| |_| |
+                     |___|_| \_|_| |_|_____/_/   \_\____/ 
 """)
 
 
-def search_news_headlines(query) -> None:
+def search_news_headlines(query, topic=args["topic"].capitalize()) -> None:
     gn = GoogleNews(lang='id', country='id')
     search = gn.search(query)
     news_item = search['entries']
@@ -41,7 +54,10 @@ def search_news_headlines(query) -> None:
             })
 
     df['source'] = source_list
-    df.to_csv('news_headlines_' + timestr + '.csv')
+    df['topic'] = topic
+
+    path = 'data/'
+    df.to_csv(path + topic + "_" + timestr + '.csv')
 
 
 if __name__ == "__main__":
